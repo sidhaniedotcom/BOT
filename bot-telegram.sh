4796d74b46d4f3bfa42cbab277db39b5c99e4d15
SIDHANIE <102436523+sidhaniedotcom@users.noreply.github.com>
2022-04-26 03:28:54 +0700

    Update bot-telegram.sh

--- bot-telegram.sh
+++ bot-telegram.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Bot telegram
 TOKEN_BOT="5329637236:AAHYFfLHarDK1Tpf3fUftwspztnWYWnbZJo"
-CHAT_ID="-454549353"
+CHAT_ID="5329637236"
  
 PESAN="Halo dikirim dari server <b>$(hostname)</b>"
 curl -s -X POST "https://api.telegram.org/bot$TOKEN_BOT/sendmessage" -d "chat_id=$CHAT_ID" -d "parse_mode=html" -d "text=$PESAN"
